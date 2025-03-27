724214d68a6ba510807252824950745b9221794f
PedroHFDA <pedrohfalbuquerque@icloud.com>
2025-03-27 18:55:54 -0300

    27/03/25

---
+++ Seção4/aula118.py
@@ -0,0 +1,16 @@
+def adiciona_clientes(nome, lista=None  ):
+    if lista is None:
+        lista = []
+    lista.append(nome)
+    return lista
+
+
+lista1 = []
+cliente1 = adiciona_clientes('Luisa',lista1)
+adiciona_clientes('Pedro', cliente1)
+print(cliente1)
+
+cliente2 = adiciona_clientes('Luciane')
+adiciona_clientes('Lucinda', cliente2)
+print(cliente2)
+#usa a mesam lista sempre
