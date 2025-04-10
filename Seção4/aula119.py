3430399878d7dc513ee52f1d461aa16fb37dfbcb
PedroHFDA <pedrohfalbuquerque@icloud.com>
2025-04-09 22:41:26 -0300

    09/04/25

--- Seção4/aula119.py
+++ Seção4/aula119.py
@@ -9,6 +9,7 @@
 # refazer = todo ['fazer café']
 # refazer = todo ['fazer café', 'caminhar']
 import os
+import json
 
 def listar(tarefas):
     print()
@@ -19,6 +20,7 @@ def listar(tarefas):
     print('Tarefas:')
     for tarefa in tarefas:
         print(f'\t{tarefa}')
+    print()
 
 
 def desfazer(tarefas, tarefas_refazer):
@@ -28,7 +30,10 @@ def desfazer(tarefas, tarefas_refazer):
         return
     
     tarefa = tarefas.pop()
+    print(f'{tarefa=} removida da lista de tarefas.')
     tarefas_refazer.append(tarefa)
+    print()
+    listar(tarefas)
 
 
 def refazer(tarefas, tarefas_refazer):
@@ -38,7 +43,10 @@ def refazer(tarefas, tarefas_refazer):
         return
     
     tarefa = tarefas_refazer.pop()
+    print(f'{tarefa=} adicionada na lista de tarefas.')
     tarefas.append(tarefa)
+    print()
+    listar(tarefas)
 
 
 def adicionar(tarefa, tarefas):
@@ -47,10 +55,33 @@ def adicionar(tarefa, tarefas):
     if not tarefa:
         print('Você não digitou nada')
         return
+    print(f'{tarefa=} adicionada na lista de tarefas.')
     tarefas.append(tarefa)
+    print()
+    listar(tarefas)
 
 
-tarefas = []
+def ler(tarefas, caminho_arquivo):
+    dados = []
+    try:
+        with open(caminho_arquivo, 'r', encoding='utf8') as arquivo:
+            dados = json.load(arquivo)
+    except FileNotFoundError:
+        print('Arquivo inexistente.')
+        salvar(tarefas, caminho_arquivo)
+    return dados
+
+
+def salvar(tarefas, caminho_arquivo):
+    dados = tarefas
+    with open(caminho_arquivo, 'w', encoding='utf8') as arquivo:
+        dados = json.dump(tarefas, arquivo, indent=2, 
+        ensure_ascii=False)
+    return dados
+
+
+CAMINHO_ARQUIVO = 'aula119.json'
+tarefas = ler([], CAMINHO_ARQUIVO)
 tarefas_refazer = []
 
 
@@ -58,16 +89,30 @@ while True:
     print('Comandos: listar, desfazer, clear, refazer e exit')
     tarefa = input('Digite uma tarefa ou comando: ')
 
+    # comandos = {
+    #     'listar': lambda: listar(tarefas),
+    #     'desfazer': lambda: desfazer(tarefas, tarefas_refazer),
+    #     'refazer': lambda: refazer(tarefas, tarefas_refazer),
+    #     'clear': lambda: os.system('clear'),
+    #     'adicionar': lambda: adicionar(tarefa, tarefas),
+    # }
+    # comando = comandos.get(tarefa) if comandos.get(tarefa) is not None else \
+    #     comandos['adicionar']
+    # comando()
+    # salvar(tarefas, CAMINHO_ARQUIVO)
+
+
+
     if tarefa == 'listar':
         listar(tarefas)
         continue
     elif tarefa == 'desfazer':
         desfazer(tarefas, tarefas_refazer)
-        listar(tarefas)
+        salvar(tarefas, CAMINHO_ARQUIVO)
         continue
     elif tarefa == 'refazer':
         refazer(tarefas, tarefas_refazer)
-        listar(tarefas)
+        salvar(tarefas, CAMINHO_ARQUIVO)
         continue
     elif tarefa == 'clear':
         os.system('cls')
@@ -78,4 +123,5 @@ while True:
     else:
         adicionar(tarefa, tarefas)
         listar(tarefas)
+        salvar(tarefas, CAMINHO_ARQUIVO)
         continue
