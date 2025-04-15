032ee29c08e004c629b81fb6c58f7a0f98b6527b
PedroHFDA <pedrohfalbuquerque@icloud.com>
2025-04-14 22:11:38 -0300

    14/04/25

---
+++ Seção4/aula127_a.py
@@ -0,0 +1,11 @@
+CAMINHO_ARQUIVO = 'aula127.json'
+
+
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+        
+p1 = Pessoa('Pedro', 23)
+p2 = Pessoa('Luciane', 52)
+p3 = Pessoa('Luísa', 22)
