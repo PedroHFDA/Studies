9a445dfa7b4db4c24bcb2f6a4556d8d4d46c3e70
PedroHFDA <pedrohfalbuquerque@icloud.com>
2025-04-16 10:56:56 -0300

    16/04/25

--- Seção4/aula127_a.py
+++ Seção4/aula127_a.py
@@ -1,4 +1,5 @@
-CAMINHO_ARQUIVO = 'aula127.json'
+import json
+CAMINHO_ARQUIVO = 'Seção4\\aula127.json'
 
 
 class Pessoa:
@@ -9,3 +10,11 @@ class Pessoa:
 p1 = Pessoa('Pedro', 23)
 p2 = Pessoa('Luciane', 52)
 p3 = Pessoa('Luísa', 22)
+bd = [vars(p1), vars(p2), vars(p3)]
+
+def fazer_dump():
+    with open(CAMINHO_ARQUIVO, 'w') as arquivo:
+        json.dump(bd, arquivo, ensure_ascii=False, indent=2)
+
+if __name__ == '__main__':
+    fazer_dump()
---
+++ Seção4/aula129.py
@@ -0,0 +1,8 @@
+class Classe:
+    @staticmethod
+    def funcao_que_esta_na_classe(*args, **kwargs):
+        print('Oi', args, kwargs)
+
+c1 = Classe()
+c1.funcao_que_esta_na_classe(1, 2, 3)
+Classe.funcao_que_esta_na_classe(nomeado=1)
